15aff2a6302a2d91e9bd4e3a4b550dc7eb352e90
choijunki <choijunki123@gmail.com>
2022-03-13 22:47:13 +0900

    init setting

--- src/MainPage.js
+++ src/MainPage.js
@@ -1,13 +1,9 @@
 import React, { Component } from 'react';
-import { BrowserRouter } from 'react-router-dom';
-import Router from './routers/Router';
 
 class MainPage extends Component {
     render () {
         return (
-            <BrowserRouter>
-            	<Router/>
-            </BrowserRouter>
+            <>gg</  >
         );
     }
 };
